feb8e99cdfc392bb06f69af86df9e2139cc87120
TahaFurkanC <cetinkayatahafurkan@gmail.com>
2023-12-14 14:05:44 +0100

    Assignments

--- src/week_14/assignments/Question_14_24.java
+++ src/week_14/assignments/Question_14_24.java
@@ -1,21 +1,22 @@
 package week_14.assignments;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
+
 
 public class Question_14_24 {
     public static void main(String[] args) {
 
         ArrayList<Integer> num = new ArrayList<>();
-        int k = 5;
 
         for (int i = 0; i < 10; i++) {
             num.add((int) (Math.random() * 20));
         }
 
         System.out.println("Array : " + num);
-        boolean result = containsNearbyDuplicate(num, k);
+        boolean result = containsNearbyDuplicate(num);
         if (result) {
             System.out.print("is there a nearby duplicate with absolute difference at most 5 ? " + result);
         } else {
@@ -25,19 +26,17 @@ public class Question_14_24 {
 
     }
 
-    public static boolean containsNearbyDuplicate(ArrayList<Integer> num, int k) {
-        Set<Integer> set = new HashSet<>();
+    public static boolean containsNearbyDuplicate(ArrayList<Integer> num) {
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < num.size(); i++) {
-            if (!set.add(num.get(i))) {
-                for (int j = 0; j < i; j++) {
-                    if (num.get(i) == num.get(j)) {
-                        if (i - j > k) {
-                            return false;
-                        } else
-                            return true;
-                    }
+            if (!map.containsKey(num.get(i))) {
+                map.put(num.get(i), i);
+            } else {
+                if (i - map.get(num.get(i)) <= 5) {
+                    return true;
+                } else {
+                    map.put(num.get(i), i);
                 }
-                return true;
             }
         }
         return false;
